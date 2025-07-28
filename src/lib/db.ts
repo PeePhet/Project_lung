008dd4f3259f36f653f30e7cf60736ebbe3d5444
PeePhet <p.teeradet@gmail.com>
2025-07-28 23:50:08 +0700

    lastest

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,21 +6,10 @@ if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
 
-let cached = global.mongoose; 
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
 
 async function connect() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+  await mongoose.connect(MONGODB_URI)
 
+}
 export default connect;
